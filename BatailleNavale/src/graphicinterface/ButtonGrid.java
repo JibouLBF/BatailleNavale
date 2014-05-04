56b377e4e4c7c32607b08296a09ddf8b4f2df28b
Vincent Abikhattar <Vincent@pc5.home>
2014-05-04 14:21:35 +0200

    buttongrid.java

---
+++ BatailleNavale/src/graphicinterface/ButtonGrid.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package graphicinterface;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Vincent
+ */
+public class ButtonGrid extends JButton{
+    private int posX;
+    private int posY;
+    
+    public ButtonGrid (int posX, int posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
+    
+    public int getPosX(){
+        return posX;
+    }
+    
+    public int getPosY(){
+        return posY;
+    }
+}
